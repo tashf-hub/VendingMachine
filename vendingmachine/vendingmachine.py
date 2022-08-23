0f0cb2df4e42eab695d926afe2c99de5a75779c9
tashf-hub <ftashtashtash@outlook.com>
2022-08-23 14:53:12 +0900

    initpy

--- vendingmachine/vendingmachine.py
+++ vendingmachine/vendingmachine.py
@@ -59,7 +59,8 @@ class VendingMachine:
         drink=drink=self.get_drink_by_name(bevarage_name)
         return 0<drink.get_stock()
     
-
+"""
 if __name__=="__main__":
     machine=VendingMachine("selling.csv")
     machine.sell(200,"cola")
+    """
